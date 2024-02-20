c3f5805d428e2f7a20496d846248f0cccfdd768c
Johepdgc <johepdg07@icloud.com>
2024-02-20 11:24:23 -0600

    cambios en js

--- script.js
+++ script.js
@@ -28,4 +28,32 @@ document.getElementById('button1').addEventListener('click', function() {
 document.getElementById('button2').addEventListener('click', function() {
     p.textContent = "¿Qué tipo de elementos insertaron por medio de js?";
     p2.textContent = "Elementos p y div";
-    img.style.display = 
+    img.style.display = 'none';
+    modal.style.display = 'block';
+});
+document.getElementById('button3').addEventListener('click', function() {
+    p.textContent = "¿Cuál es la ventaja de hacer uso del javascript al utilizar los métodos de append, appendchild e insertbefore?";
+    p2.textContent = "La ventaja es que se pueden agregar elementos al DOM de manera dinámica, es decir, sin necesidad de recargar la página.";
+    img.style.display = 'none';
+    modal.style.display = 'block';
+});
+document.getElementById('button4').addEventListener('click', function() {
+    p.textContent = "Mi Pokemón favorito es:";
+    img.src = 'images/sceptile.png';
+    img.width = 400;
+    img.height = 400;
+    p2.textContent = "Sceptile, fue el primer pokemón que tuve en mi primer juego de pokemón, es muy nostalgico.";
+    modal.style.display = 'block';
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+});
+let circle = document.getElementById('circle');
+
+window.addEventListener('mousemove', function(e) {
+    circle.style.left = e.pageX - 50 + 'px'; // Subtract half the width
+    circle.style.top = e.pageY - 50 + 'px'; // Subtract half the height
+});
